58878a7f095b5ac74c93ea1d6e7e8b24f71b0057
bhecquet <bhecquet.w@free.fr>
2024-06-07 13:54:42 +0200

    Correct exception

--- src/test/java/io/github/bhecquet/TestRealSquashInstance.java
+++ src/test/java/io/github/bhecquet/TestRealSquashInstance.java
@@ -6,6 +6,9 @@ import org.testng.SkipException;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class TestRealSquashInstance {
 
     private SquashTMApi api;
@@ -15,20 +18,19 @@ public class TestRealSquashInstance {
         String squashUrl = System.getProperty("squashUrl");
         String squashUser = System.getProperty("squashUser");
         String squashPassword = System.getProperty("squashPassword");
-        String squashProject = System.getProperty("squashProject");
 
-        if (squashUrl == null || squashUser == null || squashPassword == null || squashProject == null) {
-            throw new SkipException("Squash not configured, provide 'squashUrl', 'squashUser', 'squashPassword', 'squashProject' as properties");
+        if (squashUrl == null || squashUser == null || squashPassword == null) {
+            throw new SkipException("Squash not configured, provide 'squashUrl', 'squashUser', 'squashPassword' as properties");
         }
 
-        api = new SquashTMApi(squashUrl, squashUser, squashPassword, squashProject);
+        api = new SquashTMApi(squashUrl, squashUser, squashPassword, false);
     }
 
     @Test
     public void testCreateCampaign() {
-        Project project = api.getCurrentProject();
-        Campaign campaign = Campaign.create(project, "myCampaign", "foo/bar");
-        io.github.bhecquet.entities.Iteration iteration1 = io.github.bhecquet.entities.Iteration.create(campaign, "myIteration");
-        System.out.println(iteration1.getId());
+
+        String squashProject = System.getProperty("squashProject");
+        Project project = Project.get(squashProject);
+        Campaign.create(project, "campaignCF", (String) null, Map.of("APP", List.of("comp1", "comp2")));
     }
 }
